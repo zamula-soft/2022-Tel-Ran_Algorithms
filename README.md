d6c414b04c8dd046bcec6e9977ec261679c79e4a
Ivan Zamula <ivan.zamula@gmail.com>
2022-09-16 00:41:46 +0300

    + README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Algorithms in IT School of TEL-RAN
+
+Contains Java classes and packages with different algorithms:
+- Insertion Sort (lesson 1)
+- Bubble Sort (lesson 1)
+- Binary Search (lesson 2)
+- Recursive (lesson 2)
+- Uses binary search (lesson 3)
+
+## Additional info
+
--- src/main/java/lesson3homework/HomeWork3App.java
+++ src/main/java/lesson3homework/HomeWork3App.java
@@ -13,7 +13,7 @@ public class HomeWork3App {
 
         System.out.println("k element is " + findIndexElement(array1, array2, 7));
 
-        int [] arr = {1, 1, 2, 2, 2, 2, 3,};
+        int[] arr = {1, 1, 2, 2, 2, 2, 3,};
         int target = 2;
         System.out.println(timesInArr(arr, target));
     }
@@ -22,44 +22,37 @@ public class HomeWork3App {
 //    Первое решение выполнить рекурсивно за временную сложность O(n) - лиейная
 //    Второе решение это улучшить решение 1 до временной сложности O(log n) логарифмическая
 
-    public static int powRecursive(int x, int n)
-    {
-        if (n==1) return x;
-        return x * powRecursive(x, n -1);
+    public static int powRecursive(int x, int n) {
+        if (n == 1) return x;
+        return x * powRecursive(x, n - 1);
     }
 
-    public static int powOptimized(int x, int n)
-    {
+    public static int powOptimized(int x, int n) {
         if (n == 1)
             return x;
 
         if (n % 2 == 0)
-            return powRecursive(x * x, n/2);
+            return powRecursive(x * x, n / 2);
 
-        return x * powRecursive(x, n -1);
+        return x * powRecursive(x, n - 1);
     }
 
 
-//    Имея два отсортированных массива размера m и n соответственно, вам нужно найти элемент,
+    //    Имея два отсортированных массива размера m и n соответственно, вам нужно найти элемент,
 //    который будет находиться на k-й позиции в конечном отсортированном массиве.
 //    Массив 1 - 100 112 256 349 770
 //    Массив 2 - 72 86 113 119 265 445 892
 //    к = 7
 //    Вывод : 256
-    public static int findIndexElement(int[] array1, int[] array2, int kIndex)
-    {
+    public static int findIndexElement(int[] array1, int[] array2, int kIndex) {
         int k = 0;
         int arr1 = 0;
         int arr2 = 0;
         int next = 0;
-        while (k < kIndex)
-        {
-            if (array1[arr1] < array2[arr2])
-            {
+        while (k < kIndex) {
+            if (array1[arr1] < array2[arr2]) {
                 next = array1[arr1++];
-            }
-            else
-            {
+            } else {
                 next = array2[arr2++];
             }
             k++;
@@ -67,29 +60,24 @@ public class HomeWork3App {
         return next;
     }
 
-//    3.Имея отсортированный массив arr[] и число x, напишите функцию, которая подсчитывает вхождения x в arr[].
+    //    3.Имея отсортированный массив arr[] и число x, напишите функцию, которая подсчитывает вхождения x в arr[].
 //    Ожидаемая временная сложность O(log n)
 //    arr[] = {1, 1, 2, 2, 2, 2, 3,}
 //    x = 2
 //    Вывод: 4 раза
-    public static int timesInArr(int[] array, int x)
-    {
+    public static int timesInArr(int[] array, int x) {
         int left = 0;
         int right = array.length;
         int mid = 0;
 
-        while (left < right)
-        {
+        while (left < right) {
             mid = left + (right - left) / 2;
             if (array[mid] == x)
                 break;
 
-            if (array[mid] < x)
-            {
+            if (array[mid] < x) {
                 left = mid + 1;
-            }
-            else
-            {
+            } else {
                 right = mid - 1;
             }
         }
@@ -97,20 +85,18 @@ public class HomeWork3App {
             return -1;
 
         int result = 1;
-        int index = mid-1;
-        while (index > 0 && array[index] == array[mid])
-        {
+        int index = mid - 1;
+        while (index > 0 && array[index] == array[mid]) {
             result += 1;
             index -= 1;
         }
 
-        index = mid+1;
-        while (index < array.length && array[index] == array[mid])
-        {
+        index = mid + 1;
+        while (index < array.length && array[index] == array[mid]) {
             result += 1;
             index += 1;
         }
-return result;
+        return result;
 
     }
 
